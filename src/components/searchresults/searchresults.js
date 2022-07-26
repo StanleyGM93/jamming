5dc9aaf7471258c9285167f6b1c39c571eee4ad3
Stanley Mair <stanleygmair@gmail.com>
2022-07-27 05:33:09 +1200

    Began adding boilerplate code to playlist, searchbar and searchresults components

---
+++ src/components/searchresults/searchresults.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {TrackList} from ""
+
+export class SearchResults extends React.Component {
+    render() {
+        <div className="SearchResults">
+            <h2>Results</h2>
+            {/* <!-- Add a TrackList component --> */}
+        </div>
+    }
+}
