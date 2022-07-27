a75830f6709bd8b3183dfcc6dc484736aa8ab9e2
Stanley Mair <stanleygmair@gmail.com>
2022-07-28 04:46:51 +1200

    Created track component with JS and CSS files

---
+++ src/components/track/track.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export class Track extends React.Component {
+    renderAction() {
+        <button className='Track-action'>{isRemoval === true ? '-' : '+'}</button>
+    }
+
+    render() {
+        <div className="Track">
+            <div className="Track-information">
+                <h3><!-- track name will go here --></h3>
+                <p><!-- track artist will go here--> | <!-- track album will go here --></p>
+            </div>
+            <button className="Track-action"><!-- + or - will go here --></button>
+        </div>
+    }
+}
