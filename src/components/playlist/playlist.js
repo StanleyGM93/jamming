d81f2ef3ba3566779d6780f817e0b090d4f75663
Stanley Mair <stanleygmair@gmail.com>
2022-08-02 05:16:16 +1200

    Created functionality to alter playlist names

--- src/components/playlist/playlist.js
+++ src/components/playlist/playlist.js
@@ -4,10 +4,19 @@ import { TrackList } from "../tracklist/tracklist";
 import './playlist.css';
 
 export class Playlist extends React.Component {
+    constructor(props) {
+        super(props)
+        this.handleNameChange = this.handleNameChange.bind(this);
+    }
+
+    handleNameChange(e) {
+        this.props.onNameChange(e.target.value);
+    }
+
     render() {
         return (
             <div className="Playlist">
-                <input defaultValue={'New Playlist'} />
+                <input defaultValue={'New Playlist'} onChange={this.handleNameChange}/>
                 <TrackList tracks={this.props.playlistTracks}
                             onRemove={this.props.onRemove}
                             isRemoval={true}/>
